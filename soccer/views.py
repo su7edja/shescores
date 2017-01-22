d3cce3bacce3dbb5fd6deb678020e33bf594f9e3
Alex <su7edja@gmail.com>
2017-01-21 19:26:05 -0800

    Created basic home page

--- soccer/views.py
+++ soccer/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Competition, Match, Team, Player
+
+
+def index(request):
+    context = {
+        'num_competitions': Competition.objects.count(),
+        'num_matches': Match.objects.count(),
+        'num_teams': Team.objects.count(),
+        'num_players': Player.objects.count(),
+    }
+    return render(request, 'index.html', context=context)
